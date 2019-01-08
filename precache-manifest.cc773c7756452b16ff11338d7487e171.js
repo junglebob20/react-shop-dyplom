f53814fc98bd76d3a5c804d2541f4ecbc157088d
Artem <artem@cherrypickgames.com>
2019-01-08 15:45:06 +0100

    Updates

---
+++ precache-manifest.cc773c7756452b16ff11338d7487e171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-shop-dyplom/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c2ff50e554b8328394c",
+    "url": "/react-shop-dyplom/static/js/runtime~main.8c2ff50e.js"
+  },
+  {
+    "revision": "58829c16bfe96287306e",
+    "url": "/react-shop-dyplom/static/js/main.58829c16.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-shop-dyplom/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "58829c16bfe96287306e",
+    "url": "/react-shop-dyplom/static/css/main.edd2d22b.chunk.css"
+  },
+  {
+    "revision": "f74a9de8f19356fae9a4ba61b6e8bc7e",
+    "url": "/react-shop-dyplom/index.html"
+  }
+];
